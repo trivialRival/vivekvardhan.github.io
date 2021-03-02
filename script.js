37feb60417257fa7f1b6f8a833fcc3f66bb3a7cb
trivialRival <37234170+trivialRival@users.noreply.github.com>
2021-03-02 23:59:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// "use strict";
+const pimg = document.querySelector('.prof');
+console.log(pimg);
+pimg.addEventListener('mouseenter', () => {
+    // console.log('working');
+    pimg.animate('@keyframe')
+});
+// pimg[0].addEventListener(se)
+
